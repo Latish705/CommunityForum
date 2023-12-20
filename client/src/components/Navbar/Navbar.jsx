2a69805a116a0294e22c0fe4a744785c21605ef9
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2023-12-21 01:52:15 +0530

    final version

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -176,77 +176,167 @@ export default function Navbar() {
                 </div>
               </div>
 
-              <Disclosure.Panel className="md:hidden">
-                <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block rounded-md px-3 py-2 text-base font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-                <div className="border-t border-gray-700 pb-3 pt-4">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={userData.avatar}
-                        alt=""
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">
-                        {userData.username}
-                      </div>
-                      <div className="text-sm font-medium leading-none text-gray-400">
-                        {userData.email}
-                      </div>
-                    </div>
-                    <span className="sm:ml-3">
-                      <button
-                        type="button"
-                        className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                      >
-                        <CheckIcon
-                          className="-ml-0.5 mr-1.5 h-5 w-5"
-                          aria-hidden="true"
-                        />
-                        Publish
-                      </button>
-                    </span>
-                    <button
-                      type="button"
-                      className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    {userDataNavigation.map((item) => (
+              {checkLogin && checkLogin ? (
+                <Disclosure.Panel className="md:hidden">
+                  <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                    {navigation.map((item) => (
                       <Disclosure.Button
                         key={item.name}
                         as="a"
                         href={item.href}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "block rounded-md px-3 py-2 text-base font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </Disclosure.Button>
                     ))}
                   </div>
-                </div>
-              </Disclosure.Panel>
+                  <div className="border-t border-gray-700 pb-3 pt-4">
+                    <div className="flex items-center px-5">
+                      <div className="flex-shrink-0">
+                        {/* <img
+                        className="h-10 w-10 rounded-full"
+                        src={userData.avatar}
+                        alt=""
+                      /> */}
+                      </div>
+                      <div className="ml-3">
+                        <div className="text-base font-medium leading-none text-white">
+                          {userData.username}
+                        </div>
+                        <div className="text-sm font-medium leading-none text-gray-400">
+                          {userData.email}
+                        </div>
+                      </div>
+                      <span className="sm:ml-3">
+                        <button
+                          type="button"
+                          onClick={() => navigate("/createpost")}
+                          className="inline-flex mx-2 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                          {/* <CheckIcon
+                          className="-ml-0.5 mr-1.5 h-5 w-5"
+                          aria-hidden="true"
+                        /> */}
+                          Create a Post
+                        </button>
+                      </span>
+
+                      <button
+                        type="button"
+                        className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      >
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">View notifications</span>
+                        {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+                      </button>
+                    </div>
+                    <div className="mt-3 space-y-1 px-2">
+                      {userDataNavigation.map((item) => (
+                        <Disclosure.Button
+                          key={item.name}
+                          as="a"
+                          href={item.href}
+                          className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      ))}
+                    </div>
+                  </div>
+                </Disclosure.Panel>
+              ) : (
+                <Disclosure.Panel className="md:hidden">
+                  <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                    {navigation.map((item) => (
+                      <Disclosure.Button
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "block rounded-md px-3 py-2 text-base font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </Disclosure.Button>
+                    ))}
+                  </div>
+                  <div className="border-t border-gray-700 pb-3 pt-4">
+                    <div className="flex items-center px-5">
+                      <div className="flex-shrink-0">
+                        {/* <img
+                          className="h-10 w-10 rounded-full"
+                          src={userData.avatar}
+                          alt=""
+                        /> */}
+                      </div>
+                      <div className="ml-3">
+                        <div className="text-base font-medium leading-none text-white">
+                          {userData.username}
+                        </div>
+                        <div className="text-sm font-medium leading-none text-gray-400">
+                          {userData.email}
+                        </div>
+                      </div>
+                      <span className="sm:ml-3">
+                        <button
+                          type="button"
+                          onClick={() => navigate("/register")}
+                          className="inline-flex mx-2 items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                          {/* <CheckIcon
+                            className="-ml-0.5 mr-1.5 h-5 w-5"
+                            aria-hidden="true"
+                          /> */}
+                          Register
+                        </button>
+                      </span>
+                      <span className="sm:ml-3">
+                        <button
+                          type="button"
+                          onClick={() => navigate("/login")}
+                          className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                          {/* <CheckIcon
+                            className="-ml-0.5 mr-1.5 h-5 w-5"
+                            aria-hidden="true"
+                          /> */}
+                          Login
+                        </button>
+                      </span>
+                      <button
+                        type="button"
+                        className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      >
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">View notifications</span>
+                        {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+                      </button>
+                    </div>
+                    {/* <div className="mt-3 space-y-1 px-2">
+                      {userDataNavigation.map((item) => (
+                        <Disclosure.Button
+                          key={item.name}
+                          as="a"
+                          href={item.href}
+                          className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                        >
+                          {item.name}
+                        </Disclosure.Button>
+                      ))}
+                    </div> */}
+                  </div>
+                </Disclosure.Panel>
+              )}
             </>
           )}
         </Disclosure>
